bd73383e5bf42cb93a0673d88f5a4974ecf24c4f
John Ashmead <john.ashmead@ashmeadsoftware.com>
2013-05-28 22:45:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThirteenthApp::Application.config.secret_key_base = 'fb7144db41e036a4c09b2ab25a4c416cac71ecaaa9de6b7eb87e9b001903200f25c3be44cf52ddd01a10dfd1eda292ecfd474bbbcef155d312853622d07f1202'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThirteenthApp::Application.config.session_store :cookie_store, key: '_thirteenth_app_session'
